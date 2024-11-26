75d22eb517190880fab8e4a067532317b0085a8d
Jeffrey Bekker <jef.bekker@gmail.com>
2024-11-26 19:50:58 +0100

    Function added Single User GET, PUT, DELETE

--- gebruikersAPI/urls.py
+++ gebruikersAPI/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('users/', views.user_list, name='user_list')
+    path('users/', views.user_list, name='user_list'),
+    path('users/<int:pk>/', views.user_detail, name='user_detail'),
 ]
