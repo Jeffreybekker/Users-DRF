8a6d90f50442b40d09c0f1ce6a8f72b842bd082b
Jeffrey Bekker <jef.bekker@gmail.com>
2024-11-26 21:42:57 +0100

    User model updated. City, age and country added

--- gebruikersAPI/models.py
+++ gebruikersAPI/models.py
@@ -3,6 +3,9 @@ from django.db import models
 # Create your models here.
 class User(models.Model):
     name = models.CharField(max_length=100)
+    age = models.IntegerField(default=0)
+    country = models.CharField(max_length=100, default="Netherlands")
+    city = models.CharField(max_length=100, default="Unknown")
     
     def __str__(self):
         return self.name
