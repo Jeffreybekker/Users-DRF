cb4ff697edbc4e83523c1a68081cab237ac34668
Jeffrey Bekker <jef.bekker@gmail.com>
2024-12-01 23:22:06 +0100

    Album model related to artist added

--- gebruikersAPI/models.py
+++ gebruikersAPI/models.py
@@ -5,3 +5,14 @@ class Artist(models.Model):
     artistID = models.AutoField(primary_key=True)
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.first_name + " " + self.last_name
+    
+class Album(models.Model):
+    albumID = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=100)
+    artistID = models.ForeignKey(Artist, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.title
